8e69d66894bb0d5ed916b21978f712d9df9c76c9
iyyneswaran <pdee5660@gmail.com>
2026-06-20 12:40:42 +0530

    RomanToInteger.py

---
+++ RomanToInteger.py
@@ -0,0 +1,26 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+        prev = 0
+        
+        for ch in reversed(s):
+            curr = values[ch]
+
+            if curr < prev:
+                result -= curr
+            else:
+                result += curr
+
+            prev = curr
+
+        return result
