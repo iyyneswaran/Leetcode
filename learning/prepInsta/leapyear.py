b179fa9332c99329064ea3f27f84ec0bc209b1ad
iyyneswaran <pdee5660@gmail.com>
2026-06-25 18:27:07 +0530

    leapyear updated

--- learning/prepInsta/leapyear.py
+++ learning/prepInsta/leapyear.py
@@ -5,9 +5,23 @@ if (year%400 == 0) or (year%4==0 and year%100!=0):
 else:
     print("Not a Leap Year")
 
-# or 
+# Ternary operator
 def is_leap_year(year):
     return True if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) else False
+
+year = int(input())
+print(f"{year} is a leap year: {is_leap_year(year)}")
+
+# calender module 
+import calendar
+def calender_leapyear(year):
+    return calendar.isleap(year)
+
+print(calender_leapyear(int(input())))
+
+
+# Lambda function to check leap year
+is_leap_year = lambda year: True if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) else False
 
 year = int(input())
 print(f"{year} is a leap year: {is_leap_year(year)}")
