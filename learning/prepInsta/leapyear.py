8d4b113c9a7795281032af55c8f881618d876d54
iyyneswaran <pdee5660@gmail.com>
2026-06-25 18:24:11 +0530

    leapyear

--- learning/prepInsta/leapyear.py
+++ learning/prepInsta/leapyear.py
@@ -4,3 +4,10 @@ if (year%400 == 0) or (year%4==0 and year%100!=0):
     print("Leap Year")
 else:
     print("Not a Leap Year")
+
+# or 
+def is_leap_year(year):
+    return True if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) else False
+
+year = int(input())
+print(f"{year} is a leap year: {is_leap_year(year)}")
