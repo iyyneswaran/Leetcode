225d0f9b7a8a08dd2ce912a8e573aa37be443c99
iyyneswaran <pdee5660@gmail.com>
2026-07-02 22:56:16 +0530

    Linked list traversing and insert a node in the beginning

---
+++ learning/LinkedList/LinkedListImplementation.py
@@ -0,0 +1,32 @@
+# Node creation
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# Linked list creation
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # traversing 
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+    
+
+    # insert at beginning 
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.head = Node(10)
+    ll.head.next = Node(20)
+    ll.head.next.next = Node(30)
+    ll.print_list()
