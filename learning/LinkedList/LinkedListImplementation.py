553e277c101b06a188c234bd3a046812f1229e70
iyyneswaran <pdee5660@gmail.com>
2026-07-03 21:13:30 +0530

    insert at the end

--- learning/LinkedList/LinkedListImplementation.py
+++ learning/LinkedList/LinkedListImplementation.py
@@ -24,6 +24,20 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    # insert at the end
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+
+        temp.next = new_node
+
 if __name__ == "__main__":
     ll = LinkedList()
     ll.head = Node(10)
