fecdf142b24c124cca5e0445f14b64788c9423a5
iyyneswaran <pdee5660@gmail.com>
2026-07-11 23:19:46 +0530

    Linked list: insert in beginning and end

--- learning/LinkedList/Doubly_Linked_List/DLL_implementation.py
+++ learning/LinkedList/Doubly_Linked_List/DLL_implementation.py
@@ -23,3 +23,23 @@ class DoublyLinkedList:
         while current:
             print(current.data, end=" ")
             current = current.prev
+
+    def insert_beginning(self, data):
+        new = Node(data)
+        if self.head:
+            new.next = self.head
+            self.head.prev = new
+        self.head = new
+
+    def insert_end(self, data):
+        new = Node(data)
+        if self.head is None:
+            self.head = new
+            return
+
+        current = self.head
+        while current.next:
+            current = current.next
+
+        current.next = new
+        new.prev = current
