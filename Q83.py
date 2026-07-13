0a580da742ed744c09367cc7cada69ce045fd860
iyyneswaran <pdee5660@gmail.com>
2026-07-13 22:00:29 +0530

    Leetcode: Q83: Remove duplicates from sorted list

---
+++ Q83.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        prev = head
+        curr = head.next
+
+        while curr:
+            if prev.val == curr.val:
+                prev.next = curr.next
+                curr = prev.next
+            else:
+                prev = curr
+                curr = curr.next
+
+        return head
