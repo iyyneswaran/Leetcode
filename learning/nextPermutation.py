7db50cb0c969d87062e9d895701550ae15cc60e0
iyyneswaran <pdee5660@gmail.com>
2026-07-31 13:01:49 +0530

    Learning: Next permutation

---
+++ learning/nextPermutation.py
@@ -0,0 +1,24 @@
+def nextPermutation(n, nums):
+    # step 1: Find the pivot
+    i = n - 2
+    while i >= 0 and nums[i] >= nums[i + 1]:
+        i -= 1
+
+    # step 2: If pivot exists,  find next larger element:
+    if i >= 0:
+        j = n- 1
+        while nums[j] <= nums[i]:
+            j -= 1
+        nums[i], nums[j] = nums[j], nums[i]
+
+    # step 3: Reverse the suffix
+    left = i + 1
+    right = n - 1
+    while left < right:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right += 1
+
+length = int(input())
+nums = list(map(int, input().split()))
+print(nextPermutation(length, nums))
