68ef85fec46dab693c547aab10f546943c980c16
iyyneswaran <pdee5660@gmail.com>
2026-08-05 21:06:51 +0530

    Learning: Monotonic stack - find next greatest number

--- learning/Stack_Queue/monotonic_Stack/next_greatest_number.py
+++ learning/Stack_Queue/monotonic_Stack/next_greatest_number.py
@@ -1,4 +1,5 @@
-def decreasingMS(arr):
+# method 1 
+def decreasingMS(arr: list) -> list:
     stack = []
     for value in arr:
         while stack and stack[-1] < value:
@@ -7,5 +8,21 @@ def decreasingMS(arr):
 
     return stack
 
+# method 2
+def decreasingMS2(arr: list) -> list:
+    length = len(arr)
+    result = [-1] * length
+    stack = []
+
+    for i in range(length):
+        while stack and arr[stack[-1]] < arr[i]:
+            idx = stack.pop()
+            result[idx] = arr[i]
+        stack.append(i)
+
+    return result
+
+
 arr = list(map(int, input().split()))
 print(decreasingMS(arr))
+print(decreasingMS2(arr))
