51bf86373b5138a35376af68699aa6f10f0c5b0f
iyyneswaran <pdee5660@gmail.com>
2026-08-18 20:19:31 +0530

    Learning: Implementing chaining based hashmap

--- learning/hashing/chaining_based_hashmap.py
+++ learning/hashing/chaining_based_hashmap.py
@@ -19,5 +19,32 @@ class Hashmap:
         if self.size / self.capacity > 0.75:
             self._resize()
 
-    
-    
+    def get(self, key):
+        idx = self._hash(key)
+        for k, v in enumerate(self.buckets[idx]):
+            if k == key:
+                return v
+        raise KeyError(key)
+
+    def remove(self, key):
+        idx = self._hash(key)
+        bucket = self.buckets[idx]
+        for i, (k, v) in enumerate(bucket):
+            if k == key:
+                del bucket[i]
+                self.size -= 1
+                return
+        raise KeyError(key)
+
+    def contains(self, key):
+        idx = self._hash(key)
+        return any(k == key for k, v in self.buckets[idx])
+
+    def _resize(self):
+        old_buckets = self.buckets
+        self.capacity *= 2
+        self.buckets = [[] for _ in range(self.capacity)]
+        self.size = 0
+        for bucket in old_buckets:
+            for k, v in bucket:
+                self.put(k, v)
