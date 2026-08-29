3db15bb7070ac8e0b598851fd9a6d00c517be79a
iyyneswaran <pdee5660@gmail.com>
2026-08-29 12:41:49 +0530

    Learning: Array - two pointers => Remove duplicates

---
+++ learning/array/Two_Pointers/remove_duplicates.py
@@ -0,0 +1,10 @@
+# Remove duplicates from a sorted array in place
+def remove_duplicates(arr):
+    if not arr:
+        return 0
+    slow = 0
+    for fast in range(1, len(arr)):
+        if arr[fast] != arr[slow]:
+            slow += 1
+            arr[slow] = arr[fast]
+    return slow + 1
