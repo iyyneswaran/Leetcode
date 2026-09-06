accec21453ed764b70b82e5e1c7d5232cd64d2fc
iyyneswaran <pdee5660@gmail.com>
2026-09-06 12:59:52 +0530

    Learning: Array - sliding window => First negative number

--- learning/array/sliding_Window/first_negative_number.py
+++ learning/array/sliding_Window/first_negative_number.py
@@ -6,4 +6,32 @@ from collections import deque
 
 def first_negative_in_window(arr, k):
     dq = deque()
-    
+    res = []
+
+    for i, val in enumerate(arr):
+        if val < 0:
+            dq.append(i)
+
+        if dq and dq[0] <= i - k:
+            dq.popleft()
+
+        if i >= k - 1:
+            res.append(arr[dq[0]] if dq else 0)
+    return res
+
+# or 
+from collections import deque
+
+def first_negative_in_window(arr, k):
+    dq = deque()   # stores indices of negative numbers in current window
+    res = []
+    for i, val in enumerate(arr):
+        if val < 0:
+            dq.append(i)
+        if i >= k - 1:
+            if dq and dq[0] <= i - k:
+                dq.popleft()
+            res.append(arr[dq[0]] if dq else 0)
+            if dq and dq[0] == i - k + 1:
+                dq.popleft()
+    return res
